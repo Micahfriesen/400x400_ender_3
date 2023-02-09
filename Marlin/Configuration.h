14406ddc02edfa2e838ddbabfa39ca317d3a1b6c
Micahfriesen <micahandrewfriesen@gmail.com>
2023-02-09 18:35:43 -0500

    another commit

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -135,7 +135,7 @@
 //#define BLUETOOTH
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Ender 3 pro ready"
+#define CUSTOM_MACHINE_NAME "400x400 Ender 3 pro ready"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
